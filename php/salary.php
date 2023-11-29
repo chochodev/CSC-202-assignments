fdcc08225c02eb59fa8801f85a37bfba4decea3a
chochoprogrammer <michochoprogrammer@gmail.com>
2023-11-29 22:42:17 +0100

    updated with initiated salary code

--- php/salary.php
+++ php/salary.php
@@ -1,32 +1,25 @@
 <?php
 
 echo("---- SALARY GRADER IN PHP TERMINAL ----\n");
-echo("Salary grades = [\nA = 15,000\nB = 12,000\nC = 10,000\nD = 9,000\n]");
+echo("Salary grades = [\nA = 15,000\nB = 12,000\nC = 10,000\nD = 9,000\n]\n");
 while (true) {
     echo("Enter your salary grade: ");
-    $salary = trim(fgets(STDIN));
+    $salary = strtoupper(trim(fgets(STDIN)));
 
     if (in_array($salary, ['A','B','C','D'])) {
-        switch (true) {
-            case $salary >= 70:
-                echo("GRADE: A\n");
-                break;
-            case $salary >= 60:
-                echo("GRADE: B\n");
-                break;
-            case $salary >= 50:
-                echo("GRADE: C\n");
-                break;
-            case $salary >= 45:
-                echo("GRADE: D\n");
-                break;
-            default:
-                echo("GRADE: F\n");
-                break;
+        if ($salary == 'A') {
+            echo("SALARY: 15,000\n");
+        } else if ($salary == 'B') {
+            echo("SALARY: 12,000\n");
+        } else if ($salary == 'C') {
+            echo("SALARY: 10,000\n");
+        } else if ($salary == 'D') {
+            echo("SALARY: 9,000\n");
+        } else {
+            echo("SALARY: 9,000\n");
         }
-    }
-    else {
-        echo("salary grade not included\nTerminating...");
+    } else {
+        echo("Salary grade not included\nTerminating...");
         break;
     }
     
